8cee5a720168048e109953ca4305247d64da4f63
Thuruv <Thuruv@users.noreply.github.com>
2016-03-07 18:35:04 +0530

    Update views.py

--- app/views.py
+++ app/views.py
@@ -3,26 +3,35 @@ from app import app
 import requests
 from bs4 import BeautifulSoup
 
-url = "www.tehelka.com/2015/12/it-is-unfair-to-judge-nehru-on-kashmir-shashi-tharoor/"
-
+url = "http://static.cricinfo.com/rss/livescores.xml"
+teams = (
+    "Chennai", "Delhi",
+    "Punjab", "Kolkata",
+    "Mumbai", "Rajasthan",
+    "Bangalore", "Hyderabad"
+    )
+        
 @app.route('/')
 @app.route('/index')
 def index():
-	r = requests.get(url)
-
-	while r.status_code is not 200:
-	        r = requests.get(url)
-	
-	soup = BeautifulSoup(r.text)
-	matches.append(soup.title)
+        r = requests.get('http://www.thehindu.com/opinion/editorial/')
+        soup = BeautifulSoup(r.text)
+        for i in soup.find_all('h1'):
+                for j in i.find_all('a'):
+                        x = j['href']
+                r2 = requests.get(x)
+                soup = BeautifulSoup(r2.text)
+                y =  [i.text for i in soup.find_all("p")]
 	return '''
-        <html>
-          <head>
-            <title>zxxx</title>
-          </head>
-          <body>
-            <h1>''' + '<br>'.join(matches) + '''</h1>
-          </body>
-        </html>
-        '''
-    
+<html>
+  <head>
+    <title>Live IPL Score</title>
+  </head>
+  <body>
+  <h1>''' + '<br>'.join(soup.title) + '''</h1>
+    <p>''' + '<br>'.join(y) + '''</p>
+  </body>
+</html>
+'''
+
+
